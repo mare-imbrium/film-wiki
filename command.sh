128df95c2a1625ed2848c960db9ed4024ddf70da
kepler <githubkepler.50s@gishpuppy.com>
2018-02-24 11:46:58 +0530

    add sync and fetch_one

--- command.sh
+++ command.sh
@@ -5,16 +5,16 @@
 # 
 #         USAGE: ./command.sh 
 # 
-#   DESCRIPTION: several important commands: fetch, dump
+#   DESCRIPTION: several important commands: fetch, dump, sync, force_fetch and fetch_one
 # 
-#       OPTIONS: ---
+#       OPTIONS: filename containing urls
 #  REQUIREMENTS: ---
 #          BUGS: ---
 #         NOTES: ---
-#        AUTHOR: YOUR NAME (), 
+#        AUTHOR: rahul
 #  ORGANIZATION: 
 #       CREATED: 02/04/2016 23:46
-#      REVISION:  2016-03-10 10:24
+#      REVISION:  2018-02-20 12:08
 #===============================================================================
 
 dupes() {
@@ -37,11 +37,29 @@ force_fetch() {
     wc -l $file
     ./fetchmovie.sh --force -f $file
 }
+sync() {
+    # sync this database with imdb database
+    # fix any duplicate imdb in imdbID field
+    echo "This syncs wiki database with imdb database"
+    ./attachimdbttcode.sh
+    echo
+    echo "Remove any entries with multiple IMDB ids or fix them"
+    echo "Copy ./imdbmissing.tsv to imdbdata dir using only first column (imdbid). Rename to files.list"
+    echo "Goto imdbdata dir and run command.sh fetch"
+    #cut -f1 ./imdbmissing.tsv
+    cat ./imdbmissing.tsv
+}
+fetch_one() {
+    # fetch one wiki url
+    file=${1:-"url"}
+    ./fetchmovie.sh "$file"
+    # TODO extract imdbid and download that one in imdb database
+}
 
-if [[ $1 =~ ^(dump|fetch|force_fetch|dupes)$ ]]; then
+if [[ $1 =~ ^(dump|fetch|force_fetch|dupes|sync|fetch_one)$ ]]; then
   "$@"
 else
   echo "Invalid subcommand $1" >&2
-  echo "Commands are dump fetch force_fetch"
+  echo "Commands are dump fetch force_fetch sync"
   exit 1
 fi
